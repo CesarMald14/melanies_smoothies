6b37fdf056ca0096934828c6da40ddabdcdbd8c4
CesarMald14 <94774042+CesarMald14@users.noreply.github.com>
2026-04-23 18:04:17 -0500

    Smoothiefroot api test 1
    Added a new section to fetch and display nutrition information for watermelon from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col #Lesson 3.1
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -42,3 +43,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered," + ' ' + name_on_order + '!', icon="✅")
+
+
+#New section to display smoothiefroot nutrition information 
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
